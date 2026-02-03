e5ee4f25680dbe476cd7ee3b4e5ab7636eec1e67
Zadnap <88957683+zadnap@users.noreply.github.com>
2026-02-03 15:05:09 +0700

    feat(ui): add main layout in App.jsx

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,7 +1,17 @@
 import { Outlet } from 'react-router';
+import styles from './App.module.scss';
+import { Header, Footer } from '@/components';
 
 function App() {
-  return <Outlet />;
+  return (
+    <div className={styles.app}>
+      <Header isPlaying />
+      <div className={styles.outlet}>
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
