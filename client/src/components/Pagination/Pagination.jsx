c56df2ac02eb15f68bd8dd39a881145399d78d07
Zadnap <88957683+zadnap@users.noreply.github.com>
2026-02-04 09:32:25 +0700

    feat(ui): add leaderboard Pagination component

---
+++ client/src/components/Pagination/Pagination.jsx
@@ -0,0 +1,34 @@
+import { Button } from '@/components';
+import styles from './Pagination.module.scss';
+
+const Pagination = ({ totalPages, currentPage, setCurrentPage }) => {
+  return (
+    <nav aria-label="Leaderboard pages" className={styles.pagination}>
+      <p className={styles.pages} aria-live="polite">
+        Page <span>{currentPage}</span> of <span>{totalPages}</span>
+      </p>
+      <ul className={styles.actions}>
+        <li>
+          <Button
+            variant="outline"
+            disabled={currentPage === 1}
+            onClick={() => setCurrentPage((prev) => prev - 1)}
+          >
+            Previous
+          </Button>
+        </li>
+        <li>
+          <Button
+            variant="outline"
+            disabled={currentPage === totalPages}
+            onClick={() => setCurrentPage((prev) => prev + 1)}
+          >
+            Next
+          </Button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
