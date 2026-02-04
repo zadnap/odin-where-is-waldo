0e84341afd2ec551a148ea74785e737d7a1ff2f1
Zadnap <88957683+zadnap@users.noreply.github.com>
2026-02-04 09:55:02 +0700

    feat(ui): add LeaderboardPage component

--- client/src/pages/LeaderboardPage/LeaderboardPage.jsx
+++ client/src/pages/LeaderboardPage/LeaderboardPage.jsx
@@ -1,7 +1,38 @@
+import { LeaderboardFilter, Pagination, Table } from '@/components';
+import { useState } from 'react';
 import styles from './LeaderboardPage.module.scss';
 
 const LeaderboardPage = () => {
-  return <main className={styles.leaderboardPage}>Leaderboard Page</main>;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [playerFilter, setPlayerFilter] = useState('');
+  const [mapFilter, setMapFilter] = useState('all');
+
+  return (
+    <main className={styles.leaderboardPage}>
+      <section
+        className={styles.pageSection}
+        aria-labelledby="leaderboard-title"
+      >
+        <h2 className={styles.title} id="leaderboard-title">
+          Leaderboard
+        </h2>
+        <div className={styles.leaderboard}>
+          <LeaderboardFilter
+            playerFilter={playerFilter}
+            mapFilter={mapFilter}
+            setPlayerFilter={setPlayerFilter}
+            setMapFilter={setMapFilter}
+          />
+          <Table />
+          <Pagination
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+            totalPages={10}
+          />
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default LeaderboardPage;
