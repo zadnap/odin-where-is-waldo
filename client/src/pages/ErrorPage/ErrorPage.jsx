f4ca4615620a46b1c2e6b951c823ca5e2b743265
Zadnap <88957683+zadnap@users.noreply.github.com>
2026-02-04 10:07:50 +0700

    feat(ui): add ErrorPage component

--- client/src/pages/ErrorPage/ErrorPage.jsx
+++ client/src/pages/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,19 @@
+import { Link } from 'react-router';
 import styles from './ErrorPage.module.scss';
+import loadingIcon from '@/assets/images/loading-icon.png';
 
 const ErrorPage = () => {
-  return <main className={styles.errorPage}>Error Page</main>;
+  return (
+    <main className={styles.errorPage}>
+      <img className={styles.icon} src={loadingIcon} alt="" />
+      <p className={styles.message}>
+        The page you are looking for is not found
+      </p>
+      <Link to="/" className={styles.homepageLink}>
+        Go back to home page
+      </Link>
+    </main>
+  );
 };
 
 export default ErrorPage;
